73348475ce90f2287cfe89c442ff8a780147e667
Assma1504 <assmabaziz15@gamil.com>
2026-04-17 19:42:36 +0300

    last task completed

--- third_task.py
+++ third_task.py
@@ -6,10 +6,20 @@ newImg = Image.new("RGBA", myImg.size)
 imgDraw = ImageDraw.ImageDraw(newImg)
 # personName = input("Who you want to congratulate: ")
 personName = "Assma"
-imageWidth = int(myImg.width / 2)
-textCongratulation = (f"Congratulations {personName}")
-font = ImageFont.truetype("C:/Windows/Fonts/arialbd.ttf", size=30)
-imgDraw.text((imageWidth,20), textCongratulation, font = font, fill = (255,00,00,255) )
+textCongratulation = (f"Congratulations {personName} !")
+font = ImageFont.truetype("C:/Windows/Fonts/arialbd.ttf", size=20)
+textBbox = imgDraw.textbbox((0, 0), textCongratulation, font=font)
+textWidth = textBbox[2] - textBbox[0]
+textHeight = textBbox[3] - textBbox[1]
+textLocation = (int(myImg.width/2  - textWidth/2 ),int( myImg.height / 2  - textHeight / 2 ))
+imgDraw.text(textLocation, textCongratulation, font = font, fill = (255,00,00,255) )
 myImg.paste(newImg, newImg)
 
-myImg.show()
+# print(myImg.width)
+# print(myImg.height)
+# print(textWidth)
+# print(textHeight)
+# print(textLocation)
+
+# myImg.show()
+myImg.save("/images/watermark.png")
